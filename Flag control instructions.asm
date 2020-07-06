8b8ee5f84788cc3497e4ad7c7a30017ef38d831e
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-06 15:36:29 +0500

    Flag Control instruction

---
+++ Flag control instructions.asm
@@ -0,0 +1,10 @@
+clc  ;clear carry flag to 0
+stc  ;set carry flag to 1
+cmc ;reverse the carry flag 
+
+cld  ;clear direction flag to 0
+std  ;set direction flag to 1 
+
+cli ;clear interrupt flag to 0
+sti ;set interrupt flag to 0
+
