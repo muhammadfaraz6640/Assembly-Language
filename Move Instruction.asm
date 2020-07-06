e3b59b7aab4c4c65fda80fdce436cafa5d7fa36d
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-06 15:37:46 +0500

    Basic Move instruction

---
+++ Move Instruction.asm
@@ -0,0 +1,4 @@
+main proc
+    mov ax,02   ;move 02 to the ax register
+    mov cx,ax   ;move then ax value which is 02 to the cx                                    
+    ;instruction pointer increment every time because it holds the address of next instruction to be fetched
