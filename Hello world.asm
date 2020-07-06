0b3f616d76333a6f71047aa734f07e171f5912a2
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-06 15:36:55 +0500

    Basic class program updated

--- Hello world.asm
+++ Hello world.asm
@@ -1,10 +1,11 @@
-MOV ax,0061
-MOV bx,0012
-ADD ax,bx
-INC ax
-CMP ax,0100
-Je A
-Jne B
+MOV ax,0061 ;move 0061 to the ax register
+A :MOV bx,0012	; now move 0012 to the bx register
+B : ADD ax,bx   ; add ax and bx i-e 0061 and 0012
+INC ax  ;increment ax by 1
+CMP ax,0100	;compare the value of ax with 0100
+Je A  ;if the comparision result is true then execute label A
+Jne B	;if not true then execute label b
 
-A: MOV cx,5
-B: MOV cx,6
+A : MOV cx,5  ;label A with the code
+B : MOV cx,6	;label B with the code
+exit : 
