9782dea2f36f3227c3cd79687d80de61097c6eff
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-06 15:35:12 +0500

    Add instruction

---
+++ Add Instruction.asm
@@ -0,0 +1,8 @@
+main proc
+    Mov ax,02 ;move 02 to ax register
+    mov bx,03 ;move 02 to ax register
+    mov cx,04 ;move 02 to ax register
+    add ax,bx ;add ax and bx and store result in ax
+    add ax,cx ;add (ax+bx) and cx and store result in ax
+    
+    
