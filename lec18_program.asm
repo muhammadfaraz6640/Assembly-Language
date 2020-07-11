9df52715acf33971c71eac38686244bc69a2e974
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-07-11 19:13:27 +0500

    Addressing modes

---
+++ lec18_program.asm
@@ -0,0 +1,8 @@
+main proc
+    mov ax,0123        ;immediate addressing mode
+    mov bx,0150         ;immediate addressing mode
+    add ax,bx           ;register addressing mode
+    inc bx             ;register addressing mode 
+    dec ax             ;register addressing mode
+    and ax,bx          ;register addressing mode
+    mov [4210],ax      ;direct addressing mode
